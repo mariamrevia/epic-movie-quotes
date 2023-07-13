bbd30a7d166e5d89aef60bd3337680a321b26b33
mariamrevia <mariam.revia@redberry.ge>
2023-07-13 12:30:53 +0400

    feat:broadcasting

--- src/App.vue
+++ src/App.vue
@@ -2,4 +2,19 @@
   <RouterView />
 </template>
 
-<script setup></script>
+<script setup>
+import { onMounted } from 'vue'
+import { useUserStore } from '@/stores/authUser'
+import instantiatePusher from '@/helpers/instantiatePusher'
+
+const userStore = useUserStore()
+
+onMounted(async () => {
+  instantiatePusher()
+  await userStore.fetchUser()
+  console.log(userStore.user)
+  window.Echo.private(`likes.${userStore.user}`).listen('Notification', (data) => {
+    console.log(data)
+  })
+})
+</script>
